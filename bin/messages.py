1f279c19e1c215f34f22622e29c2a59350cb91aa
User8190 <83053839+User8190@users.noreply.github.com>
2021-10-24 18:33:42 +0530

    Update messages.py

--- bin/messages.py
+++ bin/messages.py
@@ -1,13 +1,14 @@
-start = "start message"
-query_error = "*Something Wrong!*🚫 \n`Reported to admin`"
-download_error = "*Can't Found This Subtitle.*⚠️\nplease try to download other"
+def messages():
+        start = "start message"
+        query_error = "*Something Wrong!*🚫 \n`Reported to admin`"
+        download_error = "*Can't Found This Subtitle.*⚠️\nplease try to download other"
 
-rate = 'Type: Bot\n'\
-        'Category: TV Series & Movies\n'\
-        'Rating: ⭐️ 5.00 (3)\n'\
-        'Language(s): English\n'\
-        'Owner: Gavindu Tharaka\n\n'\
-        'Menu URL: https://t.me/tlgrmcbot?start=sinhalasubdown_bot\n'\
-        'Rating URL: https://t.me/tlgrmcbot?start=sinhalasubdown_bot-review\n'
+        rate = 'Type: Bot\n'\
+                'Category: TV Series & Movies\n'\
+                'Rating: ⭐️ 5.00 (3)\n'\
+                'Language(s): English\n'\
+                'Owner: Gavindu Tharaka\n\n'\
+                'Menu URL: https://t.me/tlgrmcbot?start=sinhalasubdown_bot\n'\
+                'Rating URL: https://t.me/tlgrmcbot?start=sinhalasubdown_bot-review\n'
 
-admin_start_message = "*This message show only Admins*\n▪️ /admin\_on - Turn on Admin mode\n▪️ /admin\_off - Turn off Admin mode"
+        admin_start_message = "*This message show only Admins*\n▪️ /admin\_on - Turn on Admin mode\n▪️ /admin\_off - Turn off Admin mode"
